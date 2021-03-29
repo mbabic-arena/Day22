13f4e279b06c564053fb2d573462500770a9df57
mbabic-arena <m_babic@teol.net>
2021-03-30 01:07:35 +0200

    Props within Simple Component

---
+++ ex10/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Box from "./Box";
+
+function App() {
+  return (
+    <div className="App">
+      <Box size="28" />
+    </div>
+  );
+}
+
+export default App;
