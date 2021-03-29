94dc20a0dc9b04a402e1e634a1f7fd1aadabf818
mbabic-arena <m_babic@teol.net>
2021-03-30 01:06:49 +0200

    Props within Class Component

---
+++ ex09/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Flower from "./Flower";
+
+function App() {
+  return (
+    <div className="App">
+      <Flower color="red" />
+    </div>
+  );
+}
+export default App;
