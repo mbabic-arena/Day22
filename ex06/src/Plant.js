9198250872f33f89a851689edf53129db3a6852b
mbabic-arena <m_babic@teol.net>
2021-03-30 00:45:24 +0200

    Simple Plant Component

--- ex06/src/Plant.js
+++ ex06/src/Plant.js
@@ -1,20 +1,9 @@
-// src/Table.js
+// src/Plant.js
 
 import React, { Component } from "react";
 
-// Add TableHeader arrow function here
 const Plant = () => {
-  return (
-    <div>
-      <h2>This is a Plant Component!</h2>
-    </div>
-  );
+  return <h2>This is a Plant Component!</h2>;
 };
-
-class Table extends Component {
-  render() {
-    return <Plant />;
-  }
-}
 
 export default Plant;
