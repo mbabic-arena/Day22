0199a64336fc590fa6f02075e0e5b31661f16ec5
mbabic-arena <m_babic@teol.net>
2021-03-29 17:57:03 +0200

    Plant Class Component

---
+++ ex03/src/Plant.js
@@ -0,0 +1,15 @@
+// src/Plant.js
+
+import React, { Component } from "react";
+
+class Plant extends Component {
+  render() {
+    return (
+      <div className="Plants">
+        <h2>This is a Plant Component!</h2>
+      </div>
+    );
+  }
+}
+
+export default Plant;
