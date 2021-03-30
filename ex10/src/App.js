c655a61b3da831fcea728d3ad38b539c92f43291
mbabic-arena <m_babic@teol.net>
2021-03-30 09:52:15 +0200

    Props within Simple Component

--- ex10/src/App.js
+++ ex10/src/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import "./App.css";
 import Box from "./Box";
 
 function App() {
   return (
     <div className="App">
-      <Box size="28" />
+      <Box size="20" />
     </div>
   );
 }
--- ex10/src/Box.js
+++ ex10/src/Box.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 function Box(props) {
   return <h2>This is a {props.size}px Box!</h2>;
